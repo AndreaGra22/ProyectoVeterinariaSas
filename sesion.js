2c85227485fcc40522e42ac71cacf57f5133b7fd
AndreaGra22 <grajalesandrea22@gmail.com>
2023-11-27 17:03:28 -0500

    cambio alert

--- sesion.js
+++ sesion.js
@@ -14,6 +14,6 @@ button.addEventListener('click', (e) => {
         window.location="index.html";
     }
     else{
-        alert("Datos incorrectos",'','error')
+        swal("Datos incorrectos",'','error')
     }
 })
